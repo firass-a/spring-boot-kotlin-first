81024584b74ee7c7339873d492c427161afe93ac
firas cherfa <iamfirasse123@gmail.com>
2025-07-19 00:44:36 +0100

    hello world

---
+++ hello-world/src/main/kotlin/com/example/hello_world/HelloWorldController.kt
@@ -0,0 +1,15 @@
+package com.example.hello_world
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/")
+    fun helloWorld(): String
+    {
+        return "hello from firas"
+    }
+
+}
