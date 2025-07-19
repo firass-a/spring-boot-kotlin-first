d1bdc18e35cecd0039c5a768bf973cda33e06a84
firas cherfa <iamfirasse123@gmail.com>
2025-07-19 02:17:45 +0100

    hello world

--- hello-world/src/main/kotlin/com/example/hello_world/HelloWorldController.kt
+++ hello-world/src/main/kotlin/com/example/hello_world/HelloWorldController.kt
@@ -1,15 +1,13 @@
 package com.example.hello_world
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 class HelloWorldController {
     @GetMapping("/")
-    fun helloWorld(): String
-    {
-        return "hello from firas!!"
-    }
+    fun index(@RequestParam("name") name :String) = "Hello , $name!"
 
 }
